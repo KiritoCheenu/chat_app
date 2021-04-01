d688121099adb283c4f0eaca7ddaca9b66b279fe
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-04-01 21:25:47 +0530

    Firebase SDK Setup

--- lib/Screens/chat_Screen.dart
+++ lib/Screens/chat_Screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: ListView.builder(
-          itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(10),
-                child: Text('Hi'),
-              )),
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(10),
+          child: Text('Hi'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+
+          FirebaseFirestore.instance
+              .collection('chats/UQSomxCF6MiMQT0R0lKx/messages')
+              .snapshots()
+              .listen((data) {
+            print(data.docs[0]['text']);
+          });
+        },
+      ),
     );
   }
 }
