f80cbf2e76ffdf2dca2ea69c283d3f42efd83b2c
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-04-05 03:39:52 +0530

    messageBubble

--- lib/Widgets/chat/newMessage.dart
+++ lib/Widgets/chat/newMessage.dart
@@ -12,9 +12,11 @@ class _NewMessageState extends State<NewMessage> {
 
   void _sendMessage() {
     FocusScope.of(context).unfocus();
-    FirebaseFirestore.instance
-        .collection('chat')
-        .add({'text': _enteredMessage, 'createdAt': Timestamp.now()});
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+      
+    });
     _controller.clear();
     setState(() {
       _enteredMessage = '';
