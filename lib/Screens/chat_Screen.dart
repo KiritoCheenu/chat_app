c06e5d56fdbb34dc5c98d863063626888c552cdd
KiritoCheenu <61424879+srivastavachinmay@users.noreply.github.com>
2021-04-05 00:59:04 +0530

    logout

--- lib/Screens/chat_Screen.dart
+++ lib/Screens/chat_Screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme
+                  .of(context)
+                  .primaryIconTheme
+                  .color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout')
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged:(itemIdentifier){
+              if(itemIdentifier=='logout'){
+                FirebaseAuth.instance.signOut();
+              }
+            },)
+        ],
+      ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance
             .collection('chats/UQSomxCF6MiMQT0R0lKx/messages')
@@ -15,13 +49,14 @@ class ChatScreen extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           }
-          final documents=streamSnapshot.data.docs;
+          final documents = streamSnapshot.data.docs;
           return ListView.builder(
             itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(10),
-              child: Text(documents[index]['text']),
-            ),
+            itemBuilder: (ctx, index) =>
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(documents[index]['text']),
+                ),
           );
         },
       ),
@@ -29,9 +64,8 @@ class ChatScreen extends StatelessWidget {
         child: Icon(Icons.add),
         onPressed: () {
           FirebaseFirestore.instance
-              .collection('chats/UQSomxCF6MiMQT0R0lKx/messages').add({
-            'text': 'This was added by clicking the button'
-          });
+              .collection('chats/UQSomxCF6MiMQT0R0lKx/messages')
+              .add({'text': 'This was added by clicking the button'});
         },
       ),
     );
